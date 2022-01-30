82f1e70ca9204965e7b36cab92d3de6977ad89e2
Wojciech Konior <konior.wojciech@hotmail.com>
2022-01-30 17:42:50 +0100

    Added working test for gpu adding kernel

--- include/World.h
+++ include/World.h
@@ -1,9 +1,13 @@
 #ifndef _WORLD_H
 #define _WORLD_H
 
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 #include <iostream>
+#include <stdio.h>
 
-__host__ void world_say_hello();
+__host__ std::string world_say_hello();
 __global__ void addKernel(int *c, const int *a, const int *b);
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 #endif /* _WORLD_H */
--- tests/demo_test.cu
+++ tests/demo_test.cu
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
+#include <World.h>
 
-// Demonstrate some basic assertions.
-TEST(SecondTest, BasicAssertions2) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(addKernel, AddingTwoArraysTest) {
+    const int arraySize = 5;
+    const int a[arraySize] = { 1, 2, 3, 4, 5 };
+    const int b[arraySize] = { 10, 20, 30, 40, 50 };
+    int c[arraySize] = { 0 };
+    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    
+    for(int i = 0; i<arraySize; i++){
+      EXPECT_EQ(a[i]+b[i], c[i]);
+    }
+}
+
+TEST(WorldHeader, world_say_helloMethodTest) {
+    EXPECT_EQ(world_say_hello(), "World say hello");
 }
